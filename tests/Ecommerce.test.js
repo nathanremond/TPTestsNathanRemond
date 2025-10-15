62fc403c0d299a6da99bd32608b7b7c87ec5681c
nathanremond <perso.nathan.remond@gmail.com>
2025-10-15 11:44:02 +0200

    Troisième test

--- tests/Ecommerce.test.js
+++ tests/Ecommerce.test.js
@@ -14,3 +14,12 @@ test('supprime un produit du panier et vérifie que le montant total est correct
     removeFromBasket(panierVide, item1);
     expect(panierVide.totalPrice).toBe(0);
 })
+
+test('ajoute un produit au panier et vérifie que le montant total est correct et supprime un produit du panier et vérifie que le montant total est correct', () => {
+    const panierVide = new Basket();
+    const item1 = { name: "Carte mère", price: 100 };
+    addToBasket(panierVide, item1);
+    expect(panierVide.totalPrice).toBe(100);
+    removeFromBasket(panierVide, item1);
+    expect(panierVide.totalPrice).toBe(0);
+});
