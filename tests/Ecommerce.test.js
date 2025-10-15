2b42a2b379d9912e7531811c91126d0c488542f6
nathanremond <perso.nathan.remond@gmail.com>
2025-10-15 12:33:24 +0200

    Sixième test

--- tests/Ecommerce.test.js
+++ tests/Ecommerce.test.js
@@ -39,3 +39,69 @@ test("Test de la fonction payBasket", () => {
     payBasket(user, basket2);
     expect(user.balance).toBe(200);
 });
+
+
+//Fonction test 1
+function testAdd() {
+  const panierVide = new Basket();
+  const item1 = { name: "Carte mère", price: 100 };
+  addToBasket(panierVide, item1);
+  return panierVide.totalPrice === 100;
+}
+
+//Fonction test 2
+function testRemove() {
+  const panierVide = new Basket();
+  const item1 = { name: "Carte mère", price: 100 };
+  addToBasket(panierVide, item1);
+  removeFromBasket(panierVide, item1);
+  return panierVide.totalPrice === 0;
+}
+
+//Fonction test 3
+function testAddRemove() {
+  const panierVide = new Basket();
+  const item1 = { name: "Carte mère", price: 100 };
+  addToBasket(panierVide, item1);
+  if (panierVide.totalPrice !== 100) return false;
+  removeFromBasket(panierVide, item1);
+  return panierVide.totalPrice === 0;
+}
+
+//Fonction test 4
+function testTransactionAllowed() {
+  const user = new UserAccount("Alice", 500);
+  return (
+    transactionAllowed(user, 400) === true &&
+    transactionAllowed(user, 600) === false
+  );
+}
+
+//Fonction test 5
+function testPayBasket() {
+  const user = new UserAccount("John", 500);
+  const basket = new Basket([{ name: "Clavier", price: 300 }], 300);
+  const basket2 = new Basket([{ name: "Souris", price: 600 }], 600);
+
+  payBasket(user, basket);
+  if (user.balance !== 200) return false;
+
+  payBasket(user, basket2);
+  return user.balance === 200;
+}
+
+function testAppEcommerce() {
+  let success = testAdd();
+  success = success && testRemove();
+  success = success && testAddRemove();
+  success = success && testTransactionAllowed();
+  success = success && testPayBasket();
+
+  if (success) {
+    console.log("OK");
+  } else {
+    console.log("ERREUR");
+  }
+}
+
+testAppEcommerce();
